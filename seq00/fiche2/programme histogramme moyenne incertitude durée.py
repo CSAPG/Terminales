71bff87457953cff8630876c510899d452c078db
CSAPG <cynthia.poiraultgauvin@gmail.com>
2026-06-28 16:23:59 +0200

    Add files via upload

---
+++ seq00/fiche2/programme histogramme moyenne incertitude durée.py
@@ -0,0 +1,19 @@
+﻿import numpy as np
+import matplotlib.pyplot as plt
+Durée =[2.46,2.51,2.48,2.51,2.47,2.52,2.49,2.50,2.48,2.51,2.53]
+plt.figure(figsize=(12,10))
+plt.hist(Durée,bins=10,range=(2.45,2.55),align="mid",rwidth=0.9,color="g",edgecolor="black",label="durée")
+plt.title("Durée de chute d'un objet laché d'une hauteur constante")
+plt.xlabel("Durée de chute (s)")
+plt.ylabel("Fréquence")
+plt.legend()
+plt.show()
+
+Dt=np.mean(Durée)
+ecarttype=np.std(Durée)
+effectif=len(Durée)
+incertitudetype=ecarttype/np.sqrt(effectif)
+print("{0:2.55}".format("La durée moyenne :"),Dt,"s")
+print("{0:2.55}".format("L'écart-type vaut :"),ecarttype,"s")
+print("{0:2.55}".format("Le nombre de mesures vaut :"),effectif)
+print("{0:2.55}".format("L'incertitude-type de répétabilité de deltat vaut :"),incertitudetype,"s")
---
+++ seq00/fiche2/programme histogramme moyenne incertitude volume.py
@@ -0,0 +1,20 @@
+﻿import numpy as np
+import matplotlib.pyplot as plt
+Veprouvette =[49.61,49.55,50.91,50.87,48.03,50.29,48.58,48.06,50.06,50.72,48.95,49.4,49.21,49.31,49.78,48.77]
+plt.figure(figsize=(12,10))
+plt.hist(Veprouvette,bins=20,range=(45,55),align="mid",rwidth=0.9,color="r",edgecolor="yellow",label="éprouvette")
+plt.title("Volume d'eau contenu par une éprouvette graduée pour 50mL mesuré")
+plt.xlabel("Volume (mL)")
+plt.ylabel("Fréquence")
+plt.legend()
+plt.show()
+
+
+Vmoy=np.mean(Veprouvette)
+ecarttype=np.std(Veprouvette)
+effectif=len(Veprouvette)
+incertitudetype=ecarttype/np.sqrt(effectif)
+print("{0:55}".format("Le volume moyen :"),Vmoy,"mL")
+print("{0:55}".format("L'écart-type vaut :"),ecarttype,"mL")
+print("{0:55}".format("Le nombre de mesures vaut :"),effectif)
+print("{0:55}".format("L'incertitude-type de répétabilité de deltat vaut :"),incertitudetype,"mL")
