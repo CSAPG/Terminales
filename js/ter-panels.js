1478c8815c23061ca3073912c70fc5ccd08f1766
CSAPG <cynthia.poiraultgauvin@gmail.com>
2026-09-13 18:37:02 +0200

    Update ter-panels.js

--- js/ter-panels.js
+++ js/ter-panels.js
@@ -478,7 +478,8 @@ const TER_SEQUENCES = [
             href:'seq11/Seq11_Cinetique_cours.pdf' },
       { type:'link', icon:'🎓', label:'Cours interactif',
             sub:'Réalisé par Mme Poirault-Gauvin',
-            href:'seq11/seq11-cinetique_chimique.html' },
+            href:'seq11/seq11-cinetique_chimique.html' 
+               sub:'Fiche de cours en interactif', },
         ]
       },
       
